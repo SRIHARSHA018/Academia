e2f6e11e7f9d12bb25a85a3c63600aff2e6045b1
Sri Harsha Nandigam <38428383+SRIHARSHA018@users.noreply.github.com>
2023-09-02 13:49:31 +0000

    added loading animation and completed signup validations

--- frontend/src/Views/courses.jsx
+++ frontend/src/Views/courses.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
-import { Button, Card, CardContent, Typography } from "@mui/material";
+import { Button, Card, CardContent, Typography, CircularProgress } from "@mui/material";
 import axios from "axios"; // Import Axios
 
 class CoursesView extends Component {
   state = {
     courses: [],
+    isLoading: true, // Add a loading state
   };
 
   componentDidMount() {
     // Make an API call to retrieve the list of courses
     axios.get("/courses") // Replace with your actual API endpoint
       .then((response) => {
-        this.setState({ courses: response.data });
+        this.setState({ courses: response.data, isLoading: false }); // Set isLoading to false when data is loaded
       })
       .catch((error) => {
         console.error("Error fetching courses:", error);
+        this.setState({ isLoading: false }); // Set isLoading to false on error as well
       });
   }
 
@@ -24,23 +26,27 @@ class CoursesView extends Component {
         <Typography variant="h4" gutterBottom>
           Course List
         </Typography>
-        {this.state.courses.map((course) => (
-          <Card
-            key={course.id}
-            variant="outlined"
-            sx={{ marginBottom: 2, width: "400px" }}
-          >
-            <CardContent>
-              <Typography variant="h6">{course.title}</Typography>
-              <Typography variant="body2" color="text.secondary">
-                {course.description}
-              </Typography>
-              <Button variant="contained" color="primary">
-                Enroll
-              </Button>
-            </CardContent>
-          </Card>
-        ))}
+        {this.state.isLoading ? ( // Show loading animation when isLoading is true
+          <CircularProgress />
+        ) : (
+          this.state.courses.map((course) => (
+            <Card
+              key={course.id}
+              variant="outlined"
+              sx={{ marginBottom: 2, width: "400px" }}
+            >
+              <CardContent>
+                <Typography variant="h6">{course.title}</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {course.description}
+                </Typography>
+                <Button variant="contained" color="primary">
+                  Enroll
+                </Button>
+              </CardContent>
+            </Card>
+          ))
+        )}
       </div>
     );
   }
--- frontend/src/Views/signup.jsx
+++ frontend/src/Views/signup.jsx
@@ -1,15 +1,186 @@
 import React from "react";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+import {
+  Container,
+  TextField,
+  Button,
+  Typography,
+  Alert,
+  CircularProgress,
+} from "@mui/material";
+
+import validator from "validator";
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  },
+  formContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "1rem",
+    borderRadius: "8px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+  },
+  inputField: {
+    width: "100%",
+    marginBottom: "1rem",
+  },
+  submitButton: {
+    width: "100%",
+    marginTop: "1rem",
+    borderRadius: "25px",
+  },
+  title: {
+    marginBottom: "1rem",
+  },
+  error: {
+    width: "100%",
+    marginBottom: "1rem",
+  },
+};
 
 class SignUpView extends React.Component {
+  state = {
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+    isRegistered: false,
+    showError: false,
+    errorMessage: "",
+    isLoading: false,
+  };
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async handleSignUp() {
+    try {
+      const { email, name, password, confirmPassword } = this.state;
+
+      // Validate the email using the validator library
+      if (!validator.isEmail(email)) {
+        throw new Error("Invalid email address");
+      }
+
+      // Validate password and confirmPassword
+      if (!validator.isLength(password, { min: 6 })) {
+        throw new Error("Password must be at least 6 characters long");
+      }
+
+      if (password !== confirmPassword) {
+        throw new Error("Passwords do not match");
+      }
+
+      // Set loading state to true while making the signup request
+      this.setState({ isLoading: true });
+
+      // Make the signup request to your backend API
+      // Replace '/users/signup' with your actual signup endpoint
+      let signUpResponse = await axios.post("/users/signup", {
+        email,
+        name,
+        password,
+      });
+
+      //console.log(signUpResponse);
+
+      // Update the state to indicate successful registration
+      this.setState({ isRegistered: true });
+    } catch (error) {
+      //console.log(error);
+      let errorMessage = error.response
+        ? error.response.data.message
+        : error.message;
+      this.setState({
+        showError: true,
+        errorMessage,
+      });
+    } finally {
+      // Set loading state back to false after signup request is complete
+      this.setState({ isLoading: false });
+    }
+  }
+
+  handleSubmit() {
+    this.handleSignUp()
+      .then()
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   render() {
     return (
-      <div>
-        <input type="email" placeholder="Enter Email"></input>
-        <input type="text" placeholder="Enter Name"></input>
-        <input type="text" placeholder="Enter Password"></input>
-        <input type="text" placeholder="confirm password"></input>
-        <button type="submit">Register</button>
-      </div>
+      <Container style={styles.container}>
+        {this.state.isRegistered && (
+          <Navigate to={"/users/login"} replace={true}></Navigate>
+        )}
+        <div style={styles.formContainer}>
+          <Typography variant="h5" style={styles.title}>
+            Sign Up
+          </Typography>
+          {this.state.showError && (
+            <Alert severity="error" style={styles.error}>
+              {this.state.errorMessage}
+            </Alert>
+          )}
+          <TextField
+            name="email"
+            label="Email"
+            variant="outlined"
+            style={styles.inputField}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <TextField
+            name="name"
+            label="Name"
+            variant="outlined"
+            style={styles.inputField}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <TextField
+            name="password"
+            type="password"
+            label="Password"
+            variant="outlined"
+            style={styles.inputField}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <TextField
+            name="confirmPassword"
+            type="password"
+            label="Confirm Password"
+            variant="outlined"
+            style={styles.inputField}
+            onChange={(e) => this.handleChange(e)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            style={styles.submitButton}
+            onClick={() => this.handleSubmit()}
+            disabled={this.state.isLoading}
+          >
+            {this.state.isLoading ? (
+              <CircularProgress size={24} color="inherit" />
+            ) : (
+              "Register"
+            )}
+          </Button>
+        </div>
+      </Container>
     );
   }
 }
